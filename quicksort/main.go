05ca4f67778b31b70eb01c3e8597f76ab184e75a
caiges <caigesn@gmail.com>
2016-10-16 14:18:47 -0700

    Move tests for left/right equality above movement of left/right markers

--- quicksort/main.go
+++ quicksort/main.go
@@ -14,6 +14,10 @@ func partition(s []int, begin, end int) int {
 	right := end
 
 	for left < right {
+		// Deal with duplicate values. Simply move ahead left (or right)
+		if s[left] == s[right] {
+			left++
+		}
 		for s[left] < pivotVal {
 			left++
 		}
@@ -22,10 +26,6 @@ func partition(s []int, begin, end int) int {
 			right--
 		}
 		swap(s, left, right)
-
-		if s[left] == s[right] {
-			left++
-		}
 	}
 	return right
 }
@@ -34,15 +34,14 @@ func partition(s []int, begin, end int) int {
 func sort(s []int, begin, end int) {
 	if begin < end {
 		splitPoint := partition(s, begin, end)
-
 		sort(s, begin, splitPoint-1)
 		sort(s, splitPoint+1, end)
 	}
 }
 
 func main() {
-	unorderedList := []int{901, 73, 73, 278, 24, 12, 35, 23, 235, 25, 134, 123, 52, 634, 6723, 2, 82, 937, 903, 17, 10}
-
+	//unorderedList := []int{901, 73, 73, 278, 24, 12, 35, 23, 235, 25, 134, 123, 52, 634, 6723, 2, 82, 937, 903, 17, 10}
+	unorderedList := []int{1, 2, 2, 5, 2, 4}
 	fmt.Printf("Sort the following: \n%v\n", unorderedList)
 
 	sort(unorderedList, 0, len(unorderedList)-1)
